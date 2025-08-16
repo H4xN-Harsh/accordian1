b09d340376ab2e0950e5ef6ee8e15a93c8068327
H4x6677 <harsh7459x@gmail.com>
2025-08-16 14:04:49 +0530

    accordian

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Index from './component/Index.jsx'
+
+function App() {
+  return (
+    <Index/>
+  )
+}
+
+export default App
---
+++ src/component/accordian/data.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+const data = [
+    {
+        id:"1",
+        question:"What is accordian ?",
+        answer:"An accordion (in web development/UI context) is a UI (User Interface) component that lets you show and hide sections of related content"
+    },{
+        id:"2",
+        question:"Why do we use ?",
+        answer:"Useful for pages with lots of information (like FAQs, product details, settings, etc.)"
+    },{
+        id:"3",
+        question:"How it will work?",
+        answer:"By default, the content is hidden (collapsed)"
+
+
+    }
+]
+
+export default data;
