3114f1ec93884002b1327f7b2318a32e2c04b5dd
H4x6677 <harsh7459x@gmail.com>
2025-08-17 09:13:28 +0530

    accordian1

--- src/component/Index.jsx
+++ src/component/Index.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './accordian/data.js';
 
 function Index() {
+    const [selected , setSelected] = useState(null);
+    function handleSingleSelection(getCurrId){
+        setSelected(getCurrId);
+    }
+    console.log(selected)
   return (
-    <div>Index</div>
+    <div className='wrapper flex justify-center items-center'>
+        <div className='accordian '>
+            {
+                data&&data.length>0?
+                data.map(dataItem=><div className='item'>
+                    <div onClick={()=>handleSingleSelection(dataItem.id)} className='title text-center'>
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                    {
+                        selected===dataItem.id?
+                        <div>{dataItem.answer}</div>
+                        :null
+                    }
+                </div>)
+                : <div>No data found </div>
+            }
+        </div>
+    </div>
   )
 }
 
